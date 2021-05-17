748f71f6e46d4d06b2e37228f456bc39226fb69e
nlakshminarasimhamurty <nlakshminarasimhamurty@gmail.com>
2021-05-17 07:27:35 +0530

    added to new branch

---
+++ src/test/java/just/push/FirstPush.java
@@ -0,0 +1,12 @@
+package just.push;
+
+public class FirstPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hiii First push");
+
+	}
+
+}
